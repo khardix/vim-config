d9dd30f407f26155b0ef576630ebd2f2743ee485
Jan "Khardix" Staněk <khardix@gmail.com>
2015-01-31 21:07:25 +0100

    Add forgotoen linking to setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 # if in emty directory, clone self
 test -z "$(ls .)" && git clone git@github.com:khardix/vim-config.git .
 
+# link files
+test -d "$HOME/.vim" || ln -s "$PWD" "$HOME/.vim"
+test -f "$HOME/.vimrc" || ln -s "$PWD/vimrc" "$HOME/.vimrc"
+test -f "$HOME/.gvimrc" || ln -s "$PWD/gvimrc" "$HOME/.gvimrc"
+
 # Clone Vundle && install plugins
 git clone git@github.com:gmarik/Vundle.vim.git bundle/Vundle.vim
 
