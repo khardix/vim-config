73f93aca2e6c13ace57cb763212f0d7db0480fb1
Jan "Khardix" Staněk <khardix@gmail.com>
2016-05-21 18:59:09 +0200

    YCM: Make c++14 and c11 the default standards

--- plugin/ycm_extra_conf.py
+++ plugin/ycm_extra_conf.py
@@ -12,8 +12,8 @@ flags = [
 
 def FlagsForFile(filename, **kwargs):
   if re.match(r'\.c$', filename):
-    flags.extend(['-std=c99', '-x', 'c'])
+    flags.extend(['-std=c11', '-x', 'c'])
   else:
-    flags.extend(['-std=c++11', '-x', 'c++'])
+    flags.extend(['-std=c++14', '-x', 'c++'])
 
   return { 'flags': flags, 'do_cache': True }
